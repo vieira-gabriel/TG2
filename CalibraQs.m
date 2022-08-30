2bd93163f6010dd26f18b3654da5a5e9be4ff9df
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2022-08-30 17:47:54 -0300

    Add files via upload

--- CalibraQs.m
+++ CalibraQs.m
@@ -1,21 +1,30 @@
+clear
+
 load_variables
 
-Ts_vector = [95, 100, 110, 120, 130];
+Ts_vector = [125];
 [rows,columns] = size(Ts_vector);
+arq = 'arq4';
 
 for c = 1:columns
-    this_value = Ts_vector(1,c);
-    disp(['Runing Ts ', num2str(this_value), '%'])
-    Tm = Tb*(this_value/100);
+    disp(['Runing Ts ', num2str(Ts_vector(1,c)), '%'])
+    Tm = Tb*(Ts_vector(1,c)/100);
     Qs = 1/((Tm*s+1)^2);
     
-    workspace_name = ['arq2_tm', num2str(this_value)];
+    for i = 1:2
+    arq = ['arq', num2str(i*2)];
+    disp(['Runing ' arq])
+    workspace_name = [arq, '_tm', num2str(Ts_vector(1,c))];
     
-    sim('arq2', Tsim);
-    
-    clear this_value
+    sim(arq, Tsim);
     
     save(['Melhorias QS 2\',workspace_name]);
     
     pause(30);
+    end
 end
+
+%sim('arq1', Tsim);
+%save('Melhorias QS 2\arq1_result');
+sim('arq3', Tsim);
+save('Melhorias QS 2\arq3_result');
