41b109af27cca2970d1c6ef78761f09a9ab1f356
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2022-09-01 18:03:21 -0300

    Prepare script to new approaches

--- CalibraQs.m
+++ CalibraQs.m
@@ -3,22 +3,26 @@ clear
 load_variables
 
 %%
-Ts_vector = [80];
+Ts_vector = [85, 120];
 [rows,columns] = size(Ts_vector);
+
+path = 'Melhorias QS 3\';
+
+Tstepdown = Tstep+(Tref/2);
 
 for c = 1:columns
     disp(['Runing Ts ', num2str(Ts_vector(1,c)), '%'])
     Tm = Tb*(Ts_vector(1,c)/100);
     Qs = 1/((Tm*s+1)^2);
     
-    for i = 2:2
+    for i = 1:2
     arq = ['arq', num2str(i*2)];
     disp(['Runing ' arq])
     workspace_name = [arq, '_tm', num2str(Ts_vector(1,c))];
     
     sim(arq, Tsim);
     
-    save(['Melhorias QS 2\',workspace_name]);
+    save([path, workspace_name]);
     
     pause(30);
     end
@@ -28,10 +32,10 @@ end
 
 disp('Runing arq 1')
 sim('arq1', Tsim);
-save('Melhorias QS 2\arq1_result');
+save([path, 'arq1_result']);
 
 %%
 
 disp('Runing arq 3')
 sim('arq3', Tsim);
-save('Melhorias QS 2\arq3_result');
+save([path, 'arq3_result']);
