2c79ffe11c37bd8f70029b907876717f882057b0
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2022-09-03 14:04:05 -0300

    Return to old structure

--- CalibraQs.m
+++ CalibraQs.m
@@ -5,6 +5,30 @@ load_variables
 path = 'Estudos Observador\Melhorias Qs 3\';
 
 %%
+Ts_vector = [80, 100, 110];
+[rows,columns] = size(Ts_vector);
+
+Tstepdown = Tstep+(Tref/2);
+
+for c = 1:columns
+    disp(['Runing Ts ', num2str(Ts_vector(1,c)), '%'])
+    Tm = Tb*(Ts_vector(1,c)/100);
+    Qs = 1/((Tm*s+1)^2);
+    
+    for i = 1:2
+    arq = ['arq', num2str(i*2)];
+    disp(['Runing ' arq])
+    workspace_name = [arq, '_tm', num2str(Ts_vector(1,c))];
+    
+    sim(arq, Tsim);
+    
+    save([path, workspace_name]);
+    
+    pause(20);
+    end
+end
+
+%%
 
 disp('Runing arq 1')
 sim('arq1', Tsim);
@@ -17,28 +41,3 @@ pause(20);
 disp('Runing arq 3')
 sim('arq3', Tsim);
 save([path, 'arq3_result']);
-pause(20);
-
-%%
-Ts_vector = [80, 100, 110];
-[rows,columns] = size(Ts_vector);
-
-Tstepdown = Tstep+(Tref/2);
-
-for c = 1:columns
-    disp(['Runing Ts ', num2str(Ts_vector(1,c)), '%'])
-    Tm = Tb*(Ts_vector(1,c)/100);
-    Qs = 1/((Tm*s+1)^2);
-    
-    for i = 1:1
-    arq = ['arq', num2str(i*2)];
-    disp(['Runing ' arq])
-    workspace_name = [arq, '_tm', num2str(Ts_vector(1,c))];
-    
-    sim(arq, Tsim);
-    
-    save([path, workspace_name]);
-    
-    pause(30);
-    end
-end
