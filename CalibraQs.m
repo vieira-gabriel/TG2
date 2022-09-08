2ca58623a2164067c0222bd9dbf1a08115a8004f
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2022-09-07 23:10:51 -0300

    Prepare to new tests

--- CalibraQs.m
+++ CalibraQs.m
@@ -2,12 +2,52 @@ clear
 
 load_variables
 
-path = 'Estudos Observador\Melhorias Qs 3\';
-
-Tstepdown = Tstep+(Tref/2);
+path = 'Estudos Observador\Melhorias Qs 4\';
 
 %%
-Ts_vector = [75, 50];
+
+disp('Runing MA')
+sim('teste_ma', Tsim);
+save([path, 'MA']);
+   
+pause(20);
+
+%%
+
+disp('Runing arq 1')
+sim('arq1', Tsim);
+save([path, 'arq1_result']);
+
+        figure
+        plot(sinalSaida_sem_filtro, 'b')
+        hold on
+        plot(Saida_Simulado, 'm')
+        plot(refSignal + inputSignal, 'k')
+        legend('Saída real','Saída simulação', 'Ref + Dist')
+        axis([0 inf -25 35]);
+        hold off
+        title([arq, num2str(Ts_vector(1,c)), '%'])
+   
+pause(20);
+
+%%
+
+disp('Runing arq 3')
+sim('arq3', Tsim);
+save([path, 'arq3_result']);
+
+        figure
+        plot(sinalSaida_sem_filtro, 'b')
+        hold on
+        plot(Saida_Simulado, 'm')
+        plot(refSignal + inputSignal, 'k')
+        legend('Saída real','Saída simulação', 'Ref + Dist')
+        axis([0 inf -25 35]);
+        hold off
+        title([arq, num2str(Ts_vector(1,c)), '%'])
+
+%%
+Ts_vector = [70, 75, 50, 120, 65, 55];
 [rows,columns] = size(Ts_vector);
    
 pause(20);
@@ -18,28 +58,60 @@ for c = 1:columns
     Qs = 1/((Tm*s+1)^2);
     
     for i = 2:2
-    arq = ['arq', num2str(i*2)];
-    disp(['Runing ' arq])
-    workspace_name = [arq, '_tm', num2str(Ts_vector(1,c))];
+        arq = ['arq', num2str(i*2)];
+        disp(['Runing ' arq])
+        workspace_name = [arq, '_tm', num2str(Ts_vector(1,c))];
     
-    sim(arq, Tsim);
+        sim(arq, Tsim);
     
-    save([path, workspace_name]);
+        save([path, workspace_name]);
+
+
+        figure
+        plot(sinalSaida_sem_filtro, 'b')
+        hold on
+        plot(Saida_Simulado, 'm')
+        plot(refSignal + inputSignal, 'k')
+        legend('Saída real','Saída simulação', 'Ref + Dist')
+        axis([0 inf -25 35]);
+        hold off
+        title([arq, num2str(Ts_vector(1,c)), '%'])
     
-    pause(20);
+        pause(20);
     end
 end
 
 %%
-
-disp('Runing arq 1')
-sim('arq1', Tsim);
-save([path, 'arq1_result']);
+Ts_vector = [45, 50, 55, 60, 65, 70, 75, 80, 85, 100, 110, 120];
+[rows,columns] = size(Ts_vector);
    
 pause(20);
 
-%%
+for c = 1:columns
+    disp(['Runing Ts ', num2str(Ts_vector(1,c)), '%'])
+    Tm = Tb*(Ts_vector(1,c)/100);
+    Qs = 1/((Tm*s+1)^2);
+    
+    for i = 1:1
+        arq = ['arq', num2str(i*2)];
+        disp(['Runing ' arq])
+        workspace_name = [arq, '_tm', num2str(Ts_vector(1,c))];
+    
+        sim(arq, Tsim);
+    
+        save([path, workspace_name]);
 
-disp('Runing arq 3')
-sim('arq3', Tsim);
-save([path, 'arq3_result']);
+
+        figure
+        plot(sinalSaida_sem_filtro, 'b')
+        hold on
+        plot(Saida_Simulado, 'm')
+        plot(refSignal + inputSignal, 'k')
+        legend('Saída real','Saída simulação', 'Ref + Dist')
+        axis([0 inf -25 35]);
+        hold off
+        title([arq, num2str(Ts_vector(1,c)), '%'])
+    
+        pause(20);
+    end
+end
--- load_variables.m
+++ load_variables.m
@@ -1,51 +1,49 @@
-% Variaveis para arquitetura 1, teste em bancada
+% Variaveis para teste em bancada
 
 clear
 
+%Parametros para simular ruido
 var_ruido = 0.47;
 erro_tipico_medicao = sqrt(var_ruido);
-ref = 10;
-Tref = 800;
-dist = 5;
-Tstep = 2*Tref;
-Thold = 20;
-Tstepdown = Tstep+Thold;
-Trefdown = Tref+Tstepdown;
-%Tsim = Tstep+Thold*2;
-Tsim = Tref+Trefdown;
-zeroBot = 25;
-cond_inicial = 0;
-Ts = 5;
+
+ref = 10;       % Amplitude do degrau de entrada do sistema
+Tref = 700;     % Instante em que o degrau de referencia eh aplicado
+dist = 5;       % Amplitude do degrau do disturbio
+Tdist = 2*Tref; % Momento em que o disturbio eh aplicado
+Thold = 400;    % Tempo em que deseja que o disturbio seja aplicado
+Tdistdown = Tdist+Thold;    % Momento em que o disturbio é retirado
+Trefdown = Tref+Tdistdown;  % Momento em que a referencia retorna para zero
+Tsim = Tref+Trefdown;       % Tempo total que sera executado o Simulink
+Ts = 5;         % Tempo de amostragem
 s = tf([1 0],1);
 
-K = 2.4673;
-t1 = 17.6991;
-t2 = 52.0833;
-tau = sqrt(t1*t2);
-lambda = 3*tau;
+%K = 2.4673;
+%t1 = 17.6991;
+%t2 = 52.0833;
+%tau = sqrt(t1*t2);
+%lambda = 3*tau;
 
-sysBot = minreal(K/((t1*s+1)*(t2*s+1)));
-Z_sysBot = c2d(sysBot, Ts, 'zoh');
+%sysBot = minreal(K/((t1*s+1)*(t2*s+1)));
+%Z_sysBot = c2d(sysBot, Ts, 'zoh');
 
-Kc = (t1+t2)/(K*lambda);
-Ti = t1 + t2;
-Td = t1*t2/Ti;
+%Kc = (t1+t2)/(K*lambda);
+%Ti = t1 + t2;
+%Td = t1*t2/Ti;
 
-P = 0.3105;
-I = 0.0044;
-D = 4.1019;
+P = 0.3105; % Valor Kp do controlador
+I = 0.0044; % Valor Ki do controlador
+D = 4.1019; % Valor Kd do controlador
 C = pid(P, I, D);
 
-Pa= 0.0565;
-Pb= 0.0192;
+Pa = 0.0565;
+Pb = 0.0192;
 Ta = 1/Pa;
 Tb = 1/Pb;
-Tm = Tb*(90/100);
-Qs = 1/((Tm*s+1)^2);
+Td = Tb*(20/100); % Td é 20% da constante de tempo do polo mais lento
+Qs = 1/((Td*s+1)^2); % Filtro do observador de distúrbio
 
 %Controlador Gs:
 Kg = 0.002677;
 
 Gs =  Kg/((s+Pa)*(s+Pb));
 G_inv_Q = Gs^(-1) * Qs;
-ObsGain = 1;
