605108f7f8c985d28a7ec85100f8d8a8fd79741f
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2022-09-08 10:37:00 -0300

    Update arq ans script

--- CalibraQs.m
+++ CalibraQs.m
@@ -6,27 +6,55 @@ path = 'Estudos Observador\Melhorias Qs 4\';
 
 %%
 
+ref = 10;       % Amplitude do degrau de entrada do sistema
+Tref = 800;     % Instante em que o degrau de referencia eh aplicado
+dist = 0;       % Amplitude do degrau do disturbio
+Tdist = 0; % Momento em que o disturbio eh aplicado
+Thold = 0;    % Tempo em que deseja que o disturbio seja aplicado
+Tdistdown = 0;    % Momento em que o disturbio é retirado
+Trefdown = Tref*2;  % Momento em que a referencia retorna para zero
+Tsim = Tref+Trefdown;       % Tempo total que sera executado o Simulink
+
+
 disp('Runing MA')
 sim('teste_ma', Tsim);
 save([path, 'MA']);
+
+        figure
+        plot(sinalSaida+24, 'b')
+        hold on
+        plot(Saida_Simulado+24, 'm')
+        plot(sinalRef + 24, 'k')
+        plot(sinalDisturbio, 'r')
+        legend('Saída real','Saída simulação', 'Sinal de entrada', 'Disturbio')
+        axis([0 inf -25 35]);
+        hold off
+        title('Malha Aberta')
    
 pause(20);
 
 %%
+
+clear
+
+load_variables
+
+path = 'Estudos Observador\Melhorias Qs 4\';
 
 disp('Runing arq 1')
 sim('arq1', Tsim);
 save([path, 'arq1_result']);
 
         figure
-        plot(sinalSaida_sem_filtro, 'b')
+        plot(sinalSaida+24, 'b')
         hold on
-        plot(Saida_Simulado, 'm')
-        plot(refSignal + inputSignal, 'k')
-        legend('Saída real','Saída simulação', 'Ref + Dist')
+        plot(Saida_Simulado+24, 'm')
+        plot(sinalRef + 24, 'k')
+        plot(sinalDisturbio, 'r')
+        legend('Saída real','Saída simulação', 'Sinal de entrada', 'Disturbio')
         axis([0 inf -25 35]);
         hold off
-        title([arq, num2str(Ts_vector(1,c)), '%'])
+        title('Arq 1')
    
 pause(20);
 
@@ -37,14 +65,15 @@ sim('arq3', Tsim);
 save([path, 'arq3_result']);
 
         figure
-        plot(sinalSaida_sem_filtro, 'b')
+        plot(sinalSaida+24, 'b')
         hold on
-        plot(Saida_Simulado, 'm')
-        plot(refSignal + inputSignal, 'k')
-        legend('Saída real','Saída simulação', 'Ref + Dist')
+        plot(Saida_Simulado+24, 'm')
+        plot(sinalRef + 24, 'k')
+        plot(sinalDisturbio, 'r')
+        legend('Saída real','Saída simulação', 'Sinal de entrada', 'Disturbio')
         axis([0 inf -25 35]);
         hold off
-        title([arq, num2str(Ts_vector(1,c)), '%'])
+        title('Arq 3')
 
 %%
 Ts_vector = [70, 75, 50, 120, 65, 55];
@@ -66,13 +95,13 @@ for c = 1:columns
     
         save([path, workspace_name]);
 
-
         figure
-        plot(sinalSaida_sem_filtro, 'b')
+        plot(sinalSaida+24, 'b')
         hold on
-        plot(Saida_Simulado, 'm')
-        plot(refSignal + inputSignal, 'k')
-        legend('Saída real','Saída simulação', 'Ref + Dist')
+        plot(Saida_Simulado+24, 'm')
+        plot(sinalRef + 24, 'k')
+        plot(sinalDisturbio, 'r')
+        legend('Saída real','Saída simulação', 'Sinal de entrada', 'Disturbio')
         axis([0 inf -25 35]);
         hold off
         title([arq, num2str(Ts_vector(1,c)), '%'])
@@ -103,11 +132,12 @@ for c = 1:columns
 
 
         figure
-        plot(sinalSaida_sem_filtro, 'b')
+        plot(sinalSaida+24, 'b')
         hold on
-        plot(Saida_Simulado, 'm')
-        plot(refSignal + inputSignal, 'k')
-        legend('Saída real','Saída simulação', 'Ref + Dist')
+        plot(Saida_Simulado+24, 'm')
+        plot(sinalRef + 24, 'k')
+        plot(sinalDisturbio, 'r')
+        legend('Saída real','Saída simulação', 'Sinal de entrada', 'Disturbio')
         axis([0 inf -25 35]);
         hold off
         title([arq, num2str(Ts_vector(1,c)), '%'])
